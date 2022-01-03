c997c517f68661954b96e92f70fd2096bacaa11f
Your Name <57889490+sergeyaks@users.noreply.github.com>
2022-01-03 13:37:08 +0500

    Error DEBUGv 3.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = 'django-insecure-*&0118t=d#eiaf^askpq$++s2uc^_6(z&$m7$1x22ia8lyvjq8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sergeykas4546.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sergeykas4546.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
